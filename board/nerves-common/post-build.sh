9e1c08bc2902cbf9b4e8a11e4dbd88296b9b8945
Andy Frank <afrankvt@gmail.com>
2018-05-10 22:17:03 -0400

    Create /tmp/ symlinks during post-build.sh
    This avoids issues with Vagrant and syncing source dirs on macOS

--- board/nerves-common/post-build.sh
+++ board/nerves-common/post-build.sh
@@ -6,4 +6,10 @@ set -e
 # Nerves common post-build hook
 #
 
+# Create /tmp/ symlinks; we do this here vs board/nerves-common/skeleton
+# to make Vagrant happy when used on macOS
+cd $TARGET_DIR/dev && ln -s ../tmp/log log
+cd $TARGET_DIR/etc && ln -s ../tmp/resolv.conf resolv.conf
+
+# scrub-targets
 $BR2_EXTERNAL_NERVES_PATH/scripts/scrub-target.sh $1
